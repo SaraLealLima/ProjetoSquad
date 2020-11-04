11f27ec810998ca867c6fa9eb5263c518eadc70d
SaraLealLima <saral.limasm@gmail.com>
2020-11-03 22:56:21 -0300

    Edição da pagina de Obesidade Infantil

--- ProjetoBoquinha/obesidadeinfantil.php
+++ ProjetoBoquinha/obesidadeinfantil.php
@@ -22,15 +22,70 @@
 
     <img class="img-principal" src="images/obinf.png" alt="imagem">
 
-    <section class="section-2">
+    <section class="section-1">
         <div class="section-1-bloco">
-            <div class="section-1-texto"> <div class="section-1-titulo">O que é a obesidade?</div> <br><br> A obesidade infantil é uma doença multifatorial caracterizada pelo excesso de gordura
+            <div class="section-1-texto"> <div class="section-titulo">O que é a obesidade?</div> <br><br> A obesidade infantil é uma doença multifatorial caracterizada pelo excesso de gordura
             corporal, que interfere na expectativa e qualidade de vida do indivíduo. Pode ser causada por
             fatores nutricionais, genéticos, psicossociais, culturais, entre outros. Está associada a riscos de
             desenvolver doenças crônicas não transmissíveis, tais como diabetes, hipertensão arterial e
             doenças cardiovasculares.</div>
         </div>
     </section>
+
+    <section class="section-2">
+        <div><img class="imagem-section-2" src="images/prescricao.png" alt="Problemas de saúde"></div>
+        <div class="section-2-texto">
+            <div class="section-titulo">As consequências</div> <br>
+            Entre os riscos da obesidade infantil, de curto e longo prazo, podemos citar:
+            <br><br>
+            <ul>
+                <li> Doenças e complicações cardiovasculares, endócrinas, ortopédicas musculoesqueléticas, dermatológicas e neurológicas.</li> <br>
+                <li> Obesidade na vida adulta, desenvolvimento precoce de hipertensão, diabetes tipo 2, doença hepática gordurosa não alcoólica, asma entre outros.</li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="section-3">
+        <div><img class="imagem-section-3" src="images/tristeza.png" alt="Tristeza"></div> 
+        <div class="section-2-texto">
+            Além disso, existem os riscos de cunho social e emocional, já que a obesidade pode
+            desencadear quadros de doenças mentais ou problemas de relacionamento, incluindo: 
+            <br><br>
+            <ul>
+                <li> Bullying, baixa autoestima, interferência na frequência escolar,
+                empregabilidade e salários na vida adulta.</li> <br>
+                <li> Excesso de custos em saúde por toda vida.</li> <br>
+                <li> Falta de qualidade de vida.</li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="section-4">
+        <div><img class="imagem-section-4" src="images/boaalimentacao.png" alt="Boa alimentação"></div> 
+        <div class="section-4-texto">
+        <div class="section-titulo">Como prevenir</div> <br>
+        Uma reeducação alimentar para toda a família pode ser o primeiro — e mais
+        importante — passo para o combate à obesidade infantil. <br>
+        Estar comprometido com a saúde e deixar de lado os alimentos ricos em gorduras e
+        açúcares é fundamental para que todos consigam manter a nova rotina de uma
+        alimentação saudável a longo prazo. <br>
+        Escolha versões integrais dos cereais e farinhas, como massas, arroz e pães.
+        Aumente o consumo de verduras e legumes, e corte do cardápio os alimentos
+        ultraprocessados, redes de fast food e os refrigerantes. <br>
+        </div>
+    </section>
+
+    <section class="section-5">
+        <div><img class="imagem-section-5" src="images/doctor.png" alt="Médica"></div> 
+        <div class="section-2-texto"> Consulte, também, um nutricionista. Ele é o profissional adequado para orientá-los em
+        relação à nova dieta, para que sejam consumidos os alimentos e nutrientes em sua
+        proporção ideal. <br> 
+        Além de uma alimentação saudável, ter uma boa amamentação na
+        fase inicial, praticar atividades físicas, ter controle do tempo de exposição às telas e
+        uma boa noite de sono contribuem para a prevenção da obesidade infantil.
+        </div>
+    </section>
+
 
 
 </body>
