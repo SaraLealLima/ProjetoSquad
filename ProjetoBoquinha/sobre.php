fef7a20e11441e3024b1ea885791f4d7099092cd
SaraLealLima <saral.limasm@gmail.com>
2021-01-17 16:13:30 -0300

    Página Sobre

--- ProjetoBoquinha/sobre.php
+++ ProjetoBoquinha/sobre.php
@@ -20,7 +20,124 @@
     </header>
 
     <main>
-        <section class="container vh-100">
+        <section class="container-fluid sobre-imagem image-responsive vh-100 section-1">
+            <div class="row">
+                <div class="card-azul col-12 mt-5">
+                    <div class="card-titulo">Sobre o Projeto Boquinha </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="container-fluid section-2">
+            <div class="row col-12">
+                <div class="col-xs-12 m-5 p-sm-5 text-center texto-sobre">
+                    <p>O projeto boquinha surgiu por meio de um dos programas da Recode, que visa gerar oportunidades e estimular a transformação social e o empoderamento  digital. <br> A Recode Pro possibilitou além do desenvolvimento tecnológico, um olhar crítico diante das questões sociais, as quais muitas vezes fazem parte do nosso cotidiano de forma direta.  <br>
+                    Acreditamos que uma boa alimentação é um dos grandes fatores para uma vida saudável, logo, o objetivo do nosso projeto é ser um site informativo e instrutivo para famílias de baixa renda que possuem crianças, fornecendo um leque de receitas nutritivas e de baixo custo que ajudam no combate a obesidade infantil. 
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="container-fluid section-3">
+            <div class="row justify-content-center">
+                <div class="col-sm-11 justify-content-center">
+                    <h3>Conheça os integrantes do projeto</h3> 
+                    <p class="lead">Membros do Squad 6 de São Paulo, responsáveis por idealizar e desenvolver o Boquinha</p><br>
+
+                    <div class="card-deck mb-5">
+                        <div class="card border-success" style="width: 18rem;">
+                            <img class="card-img-top" src="./images/integranteCosmo" alt="Cosmo Amaro">
+                            <div class="card-body">
+                                <h5 class="card-title">Cosmo Amaro</h5>
+                                <p class="card-text">Estudante da Recode Pro. Entre em contato comigo clicando no botão</p>
+                                <a href="https://www.linkedin.com/in/cosmoamaro-psicologo/" class="btn btn-outline-success">LinkedIn</a>
+                            </div>
+                        </div>
+    
+                        <div class="card border-danger" style="width: 18rem;">
+                            <img class="card-img-top" src="./images/integranteDiego" alt="Diego Carmo">
+                            <div class="card-body">
+                                <h5 class="card-title">Diego Carmo</h5>
+                                <p class="card-text">Estudante da Recode Pro. Entre em contato comigo clicando no botão</p>
+                                <a href="https://www.linkedin.com/in/diego-carmo-83a55552/" class="btn btn-outline-danger">LinkedIn</a>
+                            </div>
+                        </div>
+    
+                        <div class="card border-primary" style="width: 18rem;">
+                            <img class="card-img-top" src="./images/integranteRafael" alt="Rafael Amaral">
+                            <div class="card-body">
+                                <h5 class="card-title">Rafael Amaral</h5>
+                                <p class="card-text">Estudante da Recode Pro. Entre em contato comigo clicando no botão</p>
+                                <a href="https://www.linkedin.com/in/rafaelamaral98/" class="btn btn-outline-primary">LinkedIn</a>
+                            </div>
+                        </div>
+
+                        <div class="card border-success" style="width: 18rem;">
+                            <img class="card-img-top" src="./images/integranteSamanta" alt="Samanta Antonio">
+                            <div class="card-body">
+                                <h5 class="card-title">Samanta Antonio</h5>
+                                <p class="card-text">Estudante da Recode Pro. Entre em contato comigo clicando no botão</p>
+                                <a href="https://www.linkedin.com/in/samanta-antonio/" class="btn btn-outline-success">LinkedIn</a>
+                            </div>
+                        </div>
+
+                        <div class="card border-danger" style="width: 18rem;">
+                            <img class="card-img-top" src="./images/integranteSara" alt="Sara Leal Lima">
+                            <div class="card-body">
+                                <h5 class="card-title">Sara Leal Lima</h5>
+                                <p class="card-text">Estudante da Recode Pro. Entre em contato comigo clicando no botão</p>
+                                <a href="https://www.linkedin.com/in/sara-leal-lima/" class="btn btn-outline-danger">LinkedIn</a>
+                            </div>
+                        </div>
+
+                        <div class="card border-primary" style="width: 18rem;">
+                            <img class="card-img-top" src="./images/integranteVinicius" alt="Vinicius Brito">
+                            <div class="card-body">
+                                <h5 class="card-title">Vinicius Brito</h5>
+                                <p class="card-text">Estudante da Recode Pro. Entre em contato comigo clicando no botão</p>
+                                <a href="https://www.linkedin.com/in/vin%C3%ADcius-brito-costa-150b9b158/" class="btn btn-outline-primary">LinkedIn</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="container-fluid section-4">
+            <div class="row justify-content-center">
+                <div class="col-sm-11 justify-content-center">
+                    <h3>Conheça os parceiros do projeto boquinha</h3>
+                    <p class="lead">Para disponibilizar um conteúdo de qualidade, contamos com um time de parceiros de ponta, que por meio de seus materiais nos ajudaram a elaborar nosso projeto seguindo as diretrizes cientificas da saúde.</p><br>
+                </div>
+                <div class="card-deck mb-5">
+                        <div class="card border-primary" style="width: 18rem;">
+                            <img class="card-img-top" src="./images/recode.png" alt="Recode Pro">
+                            <div class="card-body">
+                                <h5 class="card-title">Recode Pro</h5>
+                                <p class="card-text">O Recode Pro tem como objetivos a formação e a empregabilidade de pessoas em situação de vulnerabilidade social como programadores full stack. Além do conteúdo técnico, são desenvolvidos temas como criatividade, comunicação e atuação profissional, em uma metodologia que se baseia na colaboração e na construção de projetos para a resolução de problemas sociais.</p>
+                                <a href="https://www.recodepro.org.br/" class="btn btn-outline-primary">Acesse o site</a>
+                            </div>
+                        </div>
+
+                        <div class="card border-danger" style="width: 18rem;">
+                            <img class="card-img-top" src="./images/Logo_do_CREN.png" alt="CREN">
+                            <div class="card-body">
+                                <h5 class="card-title">CREN</h5>
+                                <p class="card-text">O CREN é um centro de referência internacional na área de educação nutricional e no tratamento de distúrbios nutricionais primários (subnutrição e obesidade). É um lugar que comprova quanto bem um alguém é capaz quando suas pretensões não são excessivas e irreais, quando o objetivo não é o cumprimento de um plano delimitado, e sim favorecer a pessoa e ajudá-la em suas necessidades.</p>
+                                <a href="https://www.cren.org.br/" class="btn btn-outline-danger">Acesse o site</a>
+                            </div>
+                        </div>
+
+                        <div class="card border-success" style="width: 18rem;">
+                            <img class="card-img-top" src="./images/logo-pastoral-da-crianca.png" alt="Recode Pro">
+                            <div class="card-body">
+                                <h5 class="card-title">Pastoral da Criança</h5>
+                                <p class="card-text">A Pastoral da Criança, organismo de ação social da CNBB, alicerça sua atuação na organização da comunidade e na capacitação de líderes voluntários que ali vivem e assumem a tarefa de orientar e acompanhar as famílias vizinhas em ações básicas de saúde, educação, nutrição e cidadania tendo como objetivo o "desenvolvimento integral das crianças" promovendo em função delas.</p>
+                                <a href="https://www.pastoraldacrianca.org.br/" class="btn btn-outline-success">Acesse o site</a>
+                            </div>
+                        </div>
+                </div>
+            </div>
         </section>
     </main>
 
