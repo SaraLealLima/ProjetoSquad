109f3da7e4ef239e2433e541522cc3f4cb65ac87
Dracods <39202981+Dracods@users.noreply.github.com>
2021-01-19 17:37:10 -0300

    Perfil

--- ProjetoBoquinha/perfil.php
+++ ProjetoBoquinha/perfil.php
@@ -26,6 +26,8 @@
     <script src="script.js"></script>
 
     <title>Boquinha — Perfil</title>
+
+    
 </head>
 
 <body>
@@ -37,103 +39,164 @@
     
         <div class="perfil-main-box">
 
+
+
             <div class="perfil-main-header">
-                <h2>Crianças</h2>
-                <h4><button>Adicionar Criança <div class="perfil-main-header-plus">+</div></button></h4>
+                <h4><button class="perfil-main-dados tablinks pt-2" id='defaultOpenClick' onclick="openTab(event, 'defaultOpen')">Crianças</button></h4>
+                <h4><button class='perfil-main-adicionar tablinks' onclick="openTab(event, 'adicionar-crianca')">Adicionar Criança</button></h4>
             </div>
             <div class="perfil-main-body container-fluid">
             
+            <div class="jumbotron pagina" id="adicionar-crianca">
 
-            <div id="accordion">
+                <h1 class="display-4">Por favor, preencha os campos abaixo.</h1>
 
-                <div class="card">
-                  <div class="card-header">
-                    <a class="card-link" data-toggle="collapse" href="#collapse<?php echo 'one'?>">
-                      Nome da Criança
-                    </a>
-                    <div class="criança-ultimo-update">Ultima Atualização: <?php echo $data; ?></div>
+                <form action="" method="">
+                  <div class="form-group">
+                    <label for="nome">Nome</label>
+                    <input type="nome" class="form-control" id="nome" aria-describedby="nomeHelp" placeholder="Digite seu nome">
                   </div>
-                  <div id="collapse<?php echo 'one'?>" class="collapse show" data-parent="#accordion">
-                    <div class="card-body pt-0 px-0 custom-scroll">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Idade</th>
-                                    <th scope="col">Peso</th>
-                                    <th scope="col">Altura</th>
-                                    <th scope="col">IMC</th>
-                                    <th scope="col">Data</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        24
-                                    </td>
-                                    <td>
-                                        97
-                                    </td>
-                                    <td>
-                                        186
-                                    </td>
-                                    <td>
-                                        15
-                                    </td>
-                                    <td>
-                                        19/01/2021
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        24
-                                    </td>
-                                    <td>
-                                        97
-                                    </td>
-                                    <td>
-                                        186
-                                    </td>
-                                    <td>
-                                        15
-                                    </td>
-                                    <td>
-                                        19/01/2021
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        24
-                                    </td>
-                                    <td>
-                                        97
-                                    </td>
-                                    <td>
-                                        186
-                                    </td>
-                                    <td>
-                                        15
-                                    </td>
-                                    <td>
-                                        19/01/2021
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                  <div class="form-group">
+                    <label for="genero" class="d-block">Gênero</label>
+                    <div class="form-check d-inline">
+                        <input class="form-check-input" type="radio" name="gridRadios" id="radio" value="masculino">
+                        <label class="form-check-label" for="radio">
+                            Masculino
+                        </label>
+                    </div>
+                    <div class="form-check d-inline">
+                        <input class="form-check-input" type="radio" name="gridRadios" id="radio2" value="feminino" checked>
+                        <label class="form-check-label" for="radio2">
+                            Feminino
+                        </label>
                     </div>
                   </div>
+                  <div class="form-group">
+                    <label for="idade">Idade</label>
+                    <input type="number" class="form-control" id="idade" placeholder="Idade">
+                  </div>
+                  <div class="form-group">
+                    <label for="peso">Peso</label>
+                    <input type="number" class="form-control" id="peso" placeholder="Peso">
+                  </div>
+                  <div class="form-group">
+                    <label for="altura">Altura (Ex: 110)</label>
+                    <input type="number" class="form-control" id="altura" placeholder="Altura">
+                  </div>
+                  <button type="submit" class="btn btn-primary btn-lg">Adicionar</button>
+                </form>
+
+
+
+            </div>
+
+
+
+            <div id="defaultOpen" class="pagina">
+                <div id="accordion">
+                    <div class="card border-top-0">
+                      <div class="card-header">
+                        <a class="card-link" data-toggle="collapse" href="#collapse<?php echo 'one'?>">
+                          Nome da Criança
+                        </a>
+                        <div class="criança-ultimo-update d-lg-inline d-none">Ultima Atualização: <?php echo $data; ?></div>
+                      </div>
+                      <div id="collapse<?php echo 'one'?>" class="collapse show" data-parent="#accordion">
+                        <div class="card-body pt-0 px-0 custom-scroll">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Idade</th>
+                                        <th scope="col">Peso</th>
+                                        <th scope="col">Altura</th>
+                                        <th scope="col">IMC</th>
+                                        <th scope="col">Data</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            24
+                                        </td>
+                                        <td>
+                                            97
+                                        </td>
+                                        <td>
+                                            186
+                                        </td>
+                                        <td>
+                                            15
+                                        </td>
+                                        <td>
+                                            19/01/2021
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            24
+                                        </td>
+                                        <td>
+                                            97
+                                        </td>
+                                        <td>
+                                            186
+                                        </td>
+                                        <td>
+                                            15
+                                        </td>
+                                        <td>
+                                            19/01/2021
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            24
+                                        </td>
+                                        <td>
+                                            97
+                                        </td>
+                                        <td>
+                                            186
+                                        </td>
+                                        <td>
+                                            15
+                                        </td>
+                                        <td>
+                                            19/01/2021
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                      </div>
+                    </div>
                 </div>
-    
-
-
-
             </div>
 
 
             </div>
 
         </div>
+        
     </main>
 
+    
     <?php require('./php/footer.php') ?>
+    <script>
+        function openTab(evt, id) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("pagina");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            document.getElementById(id).style.display = "block";
+            evt.currentTarget.className += " active";
+        }
+        document.getElementById("defaultOpenClick").click();
+    </script>
 </body>
 </html>
