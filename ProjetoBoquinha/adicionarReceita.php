a29cb77a4fb4b1fe3949595fac65d79a74580a6f
DiegoCarmo <deecarmo@gmail.com>
2021-02-16 14:40:04 -0300

    Adicionar Receitas

--- ProjetoBoquinha/adicionarReceita.php
+++ ProjetoBoquinha/adicionarReceita.php
@@ -10,6 +10,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Caveat+Brush&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles.adicionarReceita.css">
     <title>Adicionar Receitas</title>
 </head>
 <?php
@@ -37,24 +38,34 @@
 
     <?php require("php/header.php")?>
     <?php include("php/logadoAdm.php") ?>
-
-    <form method="post" enctype="multipart/form-data" action="">
-        <label for="nome">Nome da receita: </label><br>
-        <input type="text" name="nome" id="nome"><br>
-        <label for="idade">Faixa etaria: </label><br>
-        <select name="idade" id="idade">
-            <option value="meses_6_8" name="meses_6_8">6 a 8 meses</option>
-            <option value="meses_8_11" name="meses_8_11">8 a 11 meses</option>
-            <option value="anos_1_2" name="anos_1_2">1 a 2 anos</option>
-            <option value="anos_2_4" name="anos_2_4">2 a 4 anos</option>
-        </select><br>
-        <label for="ingredientes">Ingredientes:</label><br>
-        <textarea style="resize: none;" name="ingredientes" id="ingredientes"></textarea><br>
-        <p style="font-size: 10px; margin-top: 0;">A cada ingrediente pular uma linha</p>
-        <label for="preparo">Preparo:</label><br>
-        <textarea style="resize: none;" name="preparo" id="preparo"></textarea><br>
-        <input type="submit" value="Enviar" name="submit">
-    </form>
+<div id="boximg">
+    <img class ="img" src="" alt="some text">
+ <div class="my-5 d-flex justify-content-center"> 
+  <div class="bloco1">
+    <div class="">
+            <form method="post" enctype="multipart/form-data" action="">
+            <label for="nome">Nome da receita: </label><br>
+            <input type="text" name="nome" id="nome"><br>
+            <label for="idade">Faixa etaria: </label><br>
+            <select name="idade" id="idade">
+                <option value="meses_6_8" name="meses_6_8">6 a 8 meses</option>
+                <option value="meses_8_11" name="meses_8_11">8 a 11 meses</option>
+                <option value="anos_1_2" name="anos_1_2">1 a 2 anos</option>
+                <option value="anos_2_4" name="anos_2_4">2 a 4 anos</option>
+            </select><br>
+            <label for="ingredientes">Ingredientes:</label><br>
+            <textarea style="resize: none;" name="ingredientes" id="ingredientes"></textarea><br>
+            <p style="font-size: 10px; margin-top: 0;">A cada ingrediente pular uma linha</p>
+            <label for="preparo">Preparo:</label><br>
+            <textarea style="resize: none;" name="preparo" id="preparo"></textarea><br>
+            <label for="nome">Nome da Editor: </label><br>
+            <input type="text" name="nome" id="nome"><br><br>        
+            <input type="submit" value="Enviar" name="submit">
+        </form>
+        </div>
+        </div>
+    </div>
+</div>    
     
 
     <?php require("php/footer.php")?>
